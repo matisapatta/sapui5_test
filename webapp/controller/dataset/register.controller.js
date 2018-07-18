16e186f5b16b99e2d683e0a1936fdd29b4db833b
Mati <mati.sapatta@gmail.com>
2018-07-18 10:33:28 -0300

    commit agregado dato

--- webapp/controller/dataset/register.controller.js
+++ webapp/controller/dataset/register.controller.js
@@ -29,8 +29,17 @@ sap.ui.define([
 			var facturas = {
 				"ID": 20,
 				"variables": [
-					{ "key": 0, "factura": "0001-0000", "sociedad": "S001" },
-					{ "key": 1, "factura": "0001-0001", "sociedad": "T001" }
+					{ "key": 0, "factura": "0001-0000", "sociedad": "S001", "comprobante":"Fact A","fechadoc":"24/03/2018","impneto":50000,"impbruto":53000,"moneda":"ARS","op":"433","fechapago":"27/03/2018" },
+					{ "key": 1, "factura": "0001-0001", "sociedad": "T001","comprobante":"Fact A","fechadoc":"25/03/2018","impneto":50000,"impbruto":53000,"moneda":"ARS","op":"433","fechapago":"27/07/2018" },
+					{ "key": 2, "factura": "0002-0001", "sociedad": "T001","comprobante":"Fact A","fechadoc":"24/04/2018","impneto":50000,"impbruto":53000,"moneda":"ARS","op":"433","fechapago":"27/07/2018" },
+					{ "key": 3, "factura": "0002-0002", "sociedad": "T001","comprobante":"Fact A","fechadoc":"13/05/2018","impneto":50000,"impbruto":53000,"moneda":"ARS","op":"433","fechapago":"27/07/2018" },
+					{ "key": 4, "factura": "0002-0003", "sociedad": "S002","comprobante":"Fact A","fechadoc":"24/07/2018","impneto":50000,"impbruto":53000,"moneda":"ARS","op":"433","fechapago":"27/07/2018" },
+					{ "key": 5, "factura": "0002-0004", "sociedad": "S001","comprobante":"Fact A","fechadoc":"24/07/2018","impneto":50000,"impbruto":53000,"moneda":"ARS","op":"433","fechapago":"27/07/2018" },
+					{ "key": 6, "factura": "0002-0005", "sociedad": "S001","comprobante":"Fact A","fechadoc":"24/07/2018","impneto":50000,"impbruto":53000,"moneda":"ARS","op":"433","fechapago":"27/07/2018" },
+					{ "key": 7, "factura": "0003-0001", "sociedad": "S002","comprobante":"Fact A","fechadoc":"24/07/2018","impneto":50000,"impbruto":53000,"moneda":"ARS","op":"433","fechapago":"27/07/2018" },
+					{ "key": 8, "factura": "0003-0002", "sociedad": "S002","comprobante":"Fact A","fechadoc":"24/07/2018","impneto":50000,"impbruto":53000,"moneda":"ARS","op":"433","fechapago":"27/07/2018" },
+					{ "key": 9, "factura": "0001-0003", "sociedad": "S002","comprobante":"Fact A","fechadoc":"24/07/2018","impneto":50000,"impbruto":53000,"moneda":"ARS","op":"433","fechapago":"27/07/2018" },
+					{ "key": 10, "factura": "0001-0011", "sociedad": "T001","comprobante":"Fact A","fechadoc":"24/07/2018","impneto":50000,"impbruto":53000,"moneda":"ARS","op":"433","fechapago":"27/07/2018" }
 				]
 			}
 			oModel.setProperty("/dataset", facturas);
